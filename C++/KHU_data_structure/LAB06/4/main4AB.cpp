b7234c8c9ffe868b88997dd977509c71d1f1ee76
Ji yeon Lee <55075257+ppjy0927@users.noreply.github.com>
2021-05-22 03:57:35 +0900

    Add files via upload

---
+++ C++/KHU_data_structure/LAB06/4/main4AB.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "UnsortedType.h"
+using namespace std;
+
+int main() {
+	UnsortedType<int> list1;
+	
+	list1.InsertItem(1);
+	list1.InsertItem(3);
+	list1.InsertItem(2);
+	list1.InsertItem(4);
+	list1.InsertItem(2);
+
+	list1.DeleteItem(2);
+
+	int num;
+	list1.ResetList();
+	for (int i = 0; i < list1.LengthIs(); i++) {
+		list1.GetNextItem(num);
+		cout << num << " ";
+	}
+	return 0;
+}
